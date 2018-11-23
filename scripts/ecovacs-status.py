9a27ab66653531b436ce9e41e3f03aa3861ff641
guillebot <gschimmel@gmail.com>
2018-11-23 20:56:12 -0300

    Update ecovacs-status.py

--- scripts/ecovacs-status.py
+++ scripts/ecovacs-status.py
@@ -3,6 +3,7 @@ import sucks
 import configparser
 from sucks.cli import *
 import paho.mqtt.publish as publish
+import json
 
 # Reading the config file. In my system /root/.config/sucks.conf
 # this config file is created by running 'sucks login'. Refer to sucks documentation.
@@ -22,7 +23,7 @@ charge_status=vacbot.charge_status
 clean_status=vacbot.clean_status
 vacuum_status=vacbot.vacuum_status
 fan_speed=vacbot.fan_speed
-components=str(vacbot.components)
+components=json.dump(vacbot.components)
 
 # Publish values for the first time
 publish.single("ecovacs/1/battery_status", battery_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
@@ -30,7 +31,7 @@ publish.single("ecovacs/1/charge_status", charge_status, hostname="192.168.1.2",
 publish.single("ecovacs/1/clean_status", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/vacuum_status", vacuum_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/fan_speed", fan_speed, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
-publish.single("ecovacs/1/components", "["+components+"]", hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 # Debug info to console
 print("Start battery status:", battery_status)
 print("Start charge status:", charge_status)
@@ -63,9 +64,9 @@ while True:
         fan_speed = vacbot.fan_speed   
         publish.single("ecovacs/1/fan_speed", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
         print("New fan speed:", fan_speed)
-    if components != str(vacbot.components):
-        components = str(vacbot.components)   
-        publish.single("ecovacs/1/components", "["+components+"]", hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+    if components != json.dump(vacbot.components):
+        components = json.dump(vacbot.components)   
+        publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
         print("New components:", components)
     time.sleep(60) # I don't know if each call to the vacbot object is putting strain on the network+xmpp or it is local
 
