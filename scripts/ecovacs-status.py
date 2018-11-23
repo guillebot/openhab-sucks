6e059ab73d299e979d3457eb41a93bcbcbc8d0e8
guillebot <gschimmel@gmail.com>
2018-11-23 18:44:02 -0300

    Update ecovacs-status.py

--- scripts/ecovacs-status.py
+++ scripts/ecovacs-status.py
@@ -28,10 +28,9 @@ components=vacbot.components
 publish.single("ecovacs/1/battery_status", battery_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/charge_status", charge_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/clean_status", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
-
 publish.single("ecovacs/1/vacuum_status", vacuum_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/fan_speed", fan_speed, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
-#publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 # Debug info to console
 print("Start battery status:", battery_status)
 print("Start charge status:", charge_status)
@@ -56,6 +55,19 @@ while True:
         clean_status = vacbot.clean_status   
         publish.single("ecovacs/1/clean_status", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
         print("New clean status:", clean_status)
+    if vacuum_status != vacbot.vacuum_status:
+        vacuum_status = vacbot.vacuum_status   
+        publish.single("ecovacs/1/vacuum_status", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+        print("New vacuum status:", vacuum_status)
+    if fan_speed != vacbot.fan_speed:
+        fan_speed = vacbot.fan_speed   
+        publish.single("ecovacs/1/fan_speed", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+        print("New fan speed:", fan_speed)
+    if components != vacbot.components:
+        components = vacbot.components   
+        publish.single("ecovacs/1/components", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+        print("New components:", components)
+
     time.sleep(5) # I don't know if each call to the vacbot object is putting strain on the network+xmpp or it is local
 
 vacbot.disconnect(wait=True) # Unnecesary. I never exit this.
