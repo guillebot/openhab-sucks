e8d2f2e4a689e34ce63616b1409a532cd0233949
guillebot <gschimmel@gmail.com>
2018-11-23 10:22:32 -0300

    Update ecovacs-status.py
    first try at mqtt publishing

--- scripts/ecovacs-status.py
+++ scripts/ecovacs-status.py
@@ -1,6 +1,8 @@
 import sucks
 import configparser
 from sucks.cli import *
+import paho.mqtt.publish as publish
+
 
 # Reading the config file. In my system /root/.config/sucks.conf
 # this config file is created by running 'sucks login'. Refer to sucks documentation.
@@ -16,6 +18,9 @@ while True:
     print(vacbot.battery_status)
     print(vacbot.charge_status)
     print(vacbot.clean_status)
+    publish.single("ecovacs/1/battery_status", vacbot.battery_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+    publish.single("ecovacs/1/charge_status", vacbot.charge_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+    publish.single("ecovacs/1/clean_status", vacbot.clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
     time.sleep(10)
 
 vacbot.disconnect(wait=True)
