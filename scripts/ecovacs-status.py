a5e3ad70206b405f0d2b096785df0b3719c12e29
guillebot <gschimmel@gmail.com>
2018-11-23 20:19:05 -0300

    Update ecovacs-status.py

--- scripts/ecovacs-status.py
+++ scripts/ecovacs-status.py
@@ -30,7 +30,7 @@ publish.single("ecovacs/1/charge_status", charge_status, hostname="192.168.1.2",
 publish.single("ecovacs/1/clean_status", clean_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/vacuum_status", vacuum_status, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 publish.single("ecovacs/1/fan_speed", fan_speed, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
-#publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
+publish.single("ecovacs/1/components", components, hostname="192.168.1.2", port=8884, client_id="ecovacs-sucks")
 # Debug info to console
 print("Start battery status:", battery_status)
 print("Start charge status:", charge_status)
